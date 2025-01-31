db15d95c518778b3462bd7c43bc0c88ff2497dc5
Matthew Adebayo <olumatty@gmail.com>
2025-01-31 11:54:08 +0100

    Header 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import TopBar from "./scenes/global/TopBar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const App = () => {
-  
+  const [theme, colorMode] = useMode();
+
   return (
     <div>
-      
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content">
+              <TopBar />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
