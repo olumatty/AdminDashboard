4d5c1189853ba10a4a80454b4920aeff0ebbdcba
Matthew Adebayo <olumatty@gmail.com>
2025-01-31 12:56:10 +0100

    header 2.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import TopBar from "./scenes/global/TopBar";
 import { ColorModeContext, useMode } from "./theme";
+import {Routes, Route} from "react-router-dom"
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+//import Team from "./scenes/team";
+//import Invoices from "./scenes/invoices";
+//import Contacts from "./scenes/conntacts";
+//import Bar from "./scenes/bar";
+//import Form from "./scenes/form";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import Faq from "./scenes/faq";
+//import Geography from "./scenes/geography";
+//import Calendar from "./scenes/calendar";
+//import Sidebar from "./scenes/global/Sidebar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -8,11 +22,27 @@ const App = () => {
   return (
     <div>
       <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}> 
           <CssBaseline />
           <div className="app">
+            <Sidebar/>
             <main className="content">
               <TopBar />
+              <Routes>
+                <Route path = "/" element={<Dashboard/>}/>
+                {/* 
+                <Route path="/team" element={<Team />} /> 
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/faq" element={<Faq />} />
+                <Route path="/geography" element={<Geography />} />
+                <Route path="/calendar" element={<Calendar />} />
+                */}
+                </Routes> 
             </main>
           </div>
         </ThemeProvider>
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 import { createContext, useState, useMemo } from "react";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
+// color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
@@ -19,7 +20,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -76,9 +77,9 @@ export const tokens = (mode) => ({
           100: "#040509",
           200: "#080b12",
           300: "#0c101b",
-          400: "#f2f0f0",
+          400: "#f2f0f0", // manually changed
           500: "#141b2d",
-          600: "#434957",
+          600: "#1F2A40",
           700: "#727681",
           800: "#a1a4ab",
           900: "#d0d1d5",
@@ -119,14 +120,15 @@ export const tokens = (mode) => ({
       }),
 });
 
+// mui theme settings
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
-
   return {
-    Palette: {
+    palette: {
       mode: mode,
       ...(mode === "dark"
         ? {
+            // palette values for dark mode
             primary: {
               main: colors.primary[500],
             },
@@ -139,10 +141,11 @@ export const themeSettings = (mode) => {
               light: colors.grey[100],
             },
             background: {
-              default: colors.grey[500],
+              default: colors.primary[500],
             },
           }
         : {
+            // palette values for light mode
             primary: {
               main: colors.primary[100],
             },
@@ -160,41 +163,37 @@ export const themeSettings = (mode) => {
           }),
     },
     typography: {
-      fontFamily: ["Source Sans 3", "serif"].join(","),
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 40,
       },
-
       h2: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 32,
       },
-
       h3: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 24,
       },
-
       h4: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 20,
       },
-
       h5: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 16,
       },
-
       h6: {
-        fontFamily: ["Source Sans 3", "serif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 14,
       },
     },
   };
 };
 
+// context for color mode
 export const ColorModeContext = createContext({
   toggleColorMode: () => {},
 });
@@ -211,6 +210,5 @@ export const useMode = () => {
   );
 
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
   return [theme, colorMode];
 };
