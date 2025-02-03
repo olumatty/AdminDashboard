af54855dbd45703c0e4044981779731241547f08
Matthew Adebayo <olumatty@gmail.com>
2025-02-03 23:48:33 +0100

    contact 2.0

--- src/scenes/contact/index.jsx
+++ src/scenes/contact/index.jsx
@@ -56,37 +56,48 @@ const Contact = () => {
   
     return (
       <Box m="20px">
-        <Header title="TEAM" subtitle="Managing the Team Members" />
+        <Header title="CONTACT" subtitle="List of contacts for future reference" />
+        
         <Box
           m="40px 0 0 0"
           height="75vh"
           sx={{
             "& .MuiDataGrid-root": {
-              border: "none",
-            },
-            "& .MuiDataGrid-cell": {
-              borderBottom: "none",
-            },
-            "& .name-column--cell": {
-              color: colors.greenAccent[300],
-            },
-            "& .MuiDataGrid-columnHeaders": {
-              backgroundColor: colors.blueAccent[700],
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-virtualScroller": {
-              backgroundColor: colors.primary[400],
-            },
-            "& .MuiDataGrid-footerContainer": {
-              borderTop: "none",
-              backgroundColor: colors.blueAccent[700],
-            },
-            "& .MuiCheckbox-root": {
-              color: `${colors.greenAccent[200]} !important`,
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${colors.grey[100]} !important`, // Toolbar button text color
             },
           }}
         >
-          <DataGrid checkboxSelection rows={mockDataContacts} columns={columns} />
+          <DataGrid 
+          checkboxSelection
+          rows={mockDataContacts}
+          columns={columns}
+          slots={{ toolbar: GridToolbar }}
+         
+          />
+          
         </Box>
       </Box>
     );
