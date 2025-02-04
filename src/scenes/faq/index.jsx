e94ad89ff336a85955c0235a21f7d1b777fbef1e
Matthew Adebayo <olumatty@gmail.com>
2025-02-04 15:22:20 +0100

    FAQ 1.0

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,25 @@
+import {Box,useTheme, Typograhy} from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {tokens} from "../../theme";
+
+const FAQ = () => { 
+    const theme = useTheme();
+    const colors= tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+        <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+            
+            </AccordionSummary>
+        </Accordion>
+
+    </Box>
+  )
+}
+
+export default FAQ
