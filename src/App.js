4b2de9a77a6746a4fc8ae78c4a5a5a41d7a50e89
Matthew Adebayo <olumatty@gmail.com>
2025-02-04 12:43:35 +0100

    Calendar 2.0

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form/index.jsx";
 //import Pie from "./scenes/pie";
 //import Faq from "./scenes/faq";
 //import Geography from "./scenes/geography";
-//import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar/index.jsx";
 //import Sidebar from "./scenes/global/Sidebar";
 
 const App = () => {
@@ -34,14 +34,13 @@ const App = () => {
                 <Route path="/contacts" element={<Contacts />}/>
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
-                
+                <Route path="/calendar" element={<Calendar />} />
                  {/*
                 <Route path="/bar" element={<Bar />} />
                 <Route path="/line" element={<Line />} />
                 <Route path="/pie" element={<Pie />} />
                 <Route path="/faq" element={<Faq />} />
                 <Route path="/geography" element={<Geography />} />
-                <Route path="/calendar" element={<Calendar />} />
                 */}
                 </Routes> 
             </main>
--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -56,7 +56,34 @@ const Calendar = () => {
           backgroundColor={colors.primary[400]}
           p="15px"
           borderRadius="4px"
-        ></Box>
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
